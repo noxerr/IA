a8d145a9e9151518a80d64acebdc55715c764c66
Dani <dani_rl94@hotmail.com>
2015-10-19 22:54:01 +0200

    added SA half working

--- Bicing/src/Subclases/EstadoInicialSA.java
+++ Bicing/src/Subclases/EstadoInicialSA.java
@@ -11,6 +11,8 @@ import IA.Bicing.Estaciones;
 import MainClasses.Bicing;
 import MainClasses.Estado;
 import MainClasses.Furgoneta;
+import MainClasses.Operadores;
+import MainClasses.OperadoresSinCoste;
 import java.util.ArrayList;
 
 /**
@@ -45,51 +47,28 @@ public class EstadoInicialSA extends Estado{
                 sumaSobraFalta -= aux;
             }
         }
+        
+        //Iniciamos con origenes y destinos random
+        int origen = 0, dest = 0;
+        for(int n = 0; n < vecCamiones.size(); n++){
+            if (origen < estacOver.size()){
+                if (dest < estacUnder.size()){
+                    OperadoresSinCoste.setOrigen(vecCamiones.get(n), estacOver.get(origen), 
+                                    difDemandaBicis);
+                    OperadoresSinCoste.setDestino1(vecCamiones.get(n), estacUnder.get(dest), 
+                            this);
+                    dest++;
+                }
+                origen++;
+            }
+        }
+         
+        
         System.out.println("Estaciones que necesitan bicis: " + estacUnder.size() + " " + estacUnder);
         String retVal = "";
         for (int f : difDemandaBicis) retVal += f + " | ";
         System.out.println("DiffDemanda: " + retVal);
        // System.out.println("Estado inicial\n----------------\n " + super.toString());
     }
-    
-    /* Primer estado inicial creado. Se asigna a cada furgoneta como origen las primeras estaciones encontradas con
-     * difDemandaBicis <0 
-    */
-    private void generaEstadoInicialv0(Estaciones e){  
-        int k = 0;
-        for (int i = 0; i < Bicing.furgos; i++){
-            this.vecCamiones.add(new Furgoneta());
-        }
-        /*
-        Bicing.e.stream().forEach((_item) -> {
-            difDemandaBicis.add(0);
-        });
-        */
-        int aux2;
-        for (Estacion e1 : Bicing.e) {
-            aux2 = e1.getNumBicicletasNext()-e1.getDemanda();
-            aux2 = aux2 > 0 ? 0 : aux2; //aux sera negatiu o 0 ja que així podem contar els diners que guanyem
-            difDemandaBicis.add(aux2);
-        }
-        Bicing.difDemandaInicial = new ArrayList(difDemandaBicis);
-        for (int i = 0; i < e.size(); i++) {
-            int aux = e.get(i).getNumBicicletasNext()- e.get(i).getDemanda();
-            if (aux > 0){
-                if(k < Bicing.furgos){
-                    vecCamiones.get(k).origen = i;
-                    vecCamiones.get(k).numBicis = aux<Bicing.maxBici?aux:Bicing.maxBici;
-                    System.out.println("i: " + i + ". get: " + vecCamiones.get(k).origen + ". bicis: " + vecCamiones.get(k).numBicis);
-                    k++;
-                }
-                estacOver.add(i);
-                sumaSobraFalta += aux;
-            }
-            else if(aux < 0){
-                estacUnder.add(i);
-                sumaSobraFalta += -aux;
-            }
-        }
-        //System.out.println("Estaciones que necesitan bicis: " + estacUnder.size() + " " + estacUnder);
-       // System.out.println("Estado inicial\n----------------\n " + super.toString());
-    }
+  
 }
