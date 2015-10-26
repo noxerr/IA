6b125c0376fcee6b8ba90553767fc6e36ba812c5
David <david.sanchez.ojeda@est.fib.upc.edu>
2015-10-26 20:23:23 +0100

    Primera implementación interfaz línea de comandos (a revisar)

--- Bicing/src/MainClasses/Bicing.java
+++ Bicing/src/MainClasses/Bicing.java
@@ -43,52 +43,13 @@ public class Bicing {
     public static int EstacionUsada = 6001;
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        
+        
         long t1,t2,t3;
-        e = new Estaciones(estac, bicis, Estaciones.EQUILIBRIUM, 1234);
-
-        Estado estatInicial;
-        Estado estatInicialSA;
+        int semilla = 1234;
+        int modo = 1;
+        e = new Estaciones(estac, bicis, Estaciones.EQUILIBRIUM, semilla);
         
-        /* HC sin costes */
-        t1 = System.nanoTime();
-        estatInicial = new EstadoInicial(e, bicis);
-        t2 = System.nanoTime();
-        BicingHillClimbingSearch(estatInicial, false, false);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
-       
-        
-        
-//        /* HC con costes */
-        t1 = System.nanoTime();
-        estatInicial = new EstadoInicial(e, bicis);
-        t2 = System.nanoTime();
-        BicingHillClimbingSearch(estatInicial, true, false);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
-//        
-//        
-//        /* HC sin costes nuevo OP*/
-        t1 = System.nanoTime();
-        estatInicial = new EstadoInicialOperadoresCambioHCSinCoste(e, bicis);
-        t2 = System.nanoTime();
-        BicingHillClimbingSearch(estatInicial, false, true);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
-//        
-//        
-//        /* HC con costes nuevo Op */
-        t1 = System.nanoTime();
-        estatInicial = new EstadoInicialOperadoresCambioHC(e, bicis);
-        t2 = System.nanoTime();
-        BicingHillClimbingSearch(estatInicial, true, true);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
-        
-        //////////////
-        ///////////
-        ////////////
         
         /**
          * Parámetros para SA
@@ -97,25 +58,149 @@ public class Bicing {
         int stiter = 100;
         int k = 15;
         double lamb = 0.001;
+
+        Estado estatInicial;
+        Estado estatInicialSA;
+        Scanner in = new Scanner(System.in);
+        int opt=-1;
         
-        /* SA sin costes */
-        t1 = System.nanoTime();
-        estatInicialSA = new EstadoInicialSASinCoste(e, bicis);
-        t2 = System.nanoTime();
-        BicingSimulatedAnnealingSearch(estatInicialSA, false, steps, stiter, k, lamb);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
+        while (opt!=0){
+            System.out.println("\n\n--------------------------------\nBienvenido a Bicing\n--------------------------------");
+            System.out.println("Selecciona una opción:");
+            System.out.println("1. Ejecutar HC sin costes");
+            System.out.println("2. Ejecutar HC con costes");
+            System.out.println("3. Ejecutar SA sin costes");
+            System.out.println("4. Ejecutar SA con costes");
+            System.out.println("5. Cambiar parámetros generales");
+            System.out.println("6. Cambiar parámetros SA");
+            System.out.println("7. Ver parámetros actuales");
+            System.out.println("0. Salir");
+            System.out.println("\nOpción: ");
+            opt = in.nextInt();
+            if(opt!=0){
+            switch(opt){
+                case 1:
+                    /* HC sin costes */
+                    t1 = System.nanoTime();
+                    estatInicial = new EstadoInicial(e, bicis);
+                    t2 = System.nanoTime();
+                    BicingHillClimbingSearch(estatInicial, false, false);
+                    t3 = System.nanoTime();
+                    printTime(t1,t2,t3);
+                    break;
+                case 2:
+                      /* HC con costes */
+                    t1 = System.nanoTime();
+                    estatInicial = new EstadoInicial(e, bicis);
+                    t2 = System.nanoTime();
+                    BicingHillClimbingSearch(estatInicial, true, false);
+                    t3 = System.nanoTime();
+                    printTime(t1,t2,t3);
+                    break;
+                case 3:
+                    /* SA sin costes */
+                    t1 = System.nanoTime();
+                    estatInicialSA = new EstadoInicialSASinCoste(e, bicis);
+                    t2 = System.nanoTime();
+                    BicingSimulatedAnnealingSearch(estatInicialSA, false, steps, stiter, k, lamb);
+                    t3 = System.nanoTime();
+                    printTime(t1,t2,t3);
+                    break;
+                case 4:
+                    /* SA con costes */
+                    t1 = System.nanoTime();
+                    estatInicialSA = new EstadoInicialSA(e, bicis);
+                    t2 = System.nanoTime();
+                    BicingSimulatedAnnealingSearch(estatInicialSA, true, steps, stiter, k, lamb);
+                    t3 = System.nanoTime();
+                    printTime(t1,t2,t3);
+                    break;
+                case 5:
+                    System.out.println("Número de estaciones:");
+                    estac = in.nextInt();
+                    System.out.println("Número de bicis:");
+                    bicis = in.nextInt();
+                    System.out.println("Número de furgonetas:");
+                    furgos = in.nextInt();
+                    System.out.println("Introduce\n1: Demanda normal\n2: Hora punta");
+                    modo = (in.nextInt()==1) ? Estaciones.EQUILIBRIUM : Estaciones.RUSH_HOUR;
+                    System.out.println("Semilla:");
+                    semilla = in.nextInt();
+                    e = new Estaciones(estac, bicis, modo, semilla);
+                    break;
+                case 6:
+                    System.out.println("Steps:");
+                    steps = in.nextInt();
+                    System.out.println("Max it:");
+                    stiter = in.nextInt();
+                    System.out.println("k:");
+                    k = in.nextInt();
+                    System.out.println("Lambda:");
+                    lamb = in.nextInt();
+                    break;
+                case 7:
+                    System.out.println("------------ GENERAL -----------------------");
+                    System.out.println("Estaciones: "+estac);
+                    System.out.println("Bicis: "+bicis);
+                    System.out.println("Furgonetas: "+furgos);
+                    System.out.println("Demanda: " + ((modo==1) ? "Equilibrada" : "Hora punta"));
+                    System.out.println("Semilla: "+semilla);
+                    System.out.println("-------------- SA --------------------------");
+                    System.out.println("Steps: "+steps);
+                    System.out.println("Max it: "+stiter);
+                    System.out.println("k: "+k);
+                    System.out.println("Lambda: "+lamb);
+                    System.out.println("--------------------------------------------");
+                    break;
+                    
+            }
+                difDemandaInicial = new ArrayList<Integer>();
+                System.out.println("Pulsa cualquier tecla para continuar...");
+                try
+                {
+                    System.in.read();
+                }  
+                catch(Exception e)
+                {}  
+            }
+            
+            
+            
+        }
         
-        /* SA con costes */
-        t1 = System.nanoTime();
-        estatInicialSA = new EstadoInicialSA(e, bicis);
-        t2 = System.nanoTime();
-        BicingSimulatedAnnealingSearch(estatInicialSA, true, steps, stiter, k, lamb);
-        t3 = System.nanoTime();
-        printTime(t1,t2,t3);
+        
+        
+        
+       
+        
+     
+////        /* HC sin costes nuevo OP*/
+//        t1 = System.nanoTime();
+//        estatInicial = new EstadoInicialOperadoresCambioHCSinCoste(e, bicis);
+//        t2 = System.nanoTime();
+//        BicingHillClimbingSearch(estatInicial, false, true);
+//        t3 = System.nanoTime();
+//        printTime(t1,t2,t3);
+////        
+////        
+////        /* HC con costes nuevo Op */
+//        t1 = System.nanoTime();
+//        estatInicial = new EstadoInicialOperadoresCambioHC(e, bicis);
+//        t2 = System.nanoTime();
+//        BicingHillClimbingSearch(estatInicial, true, true);
+//        t3 = System.nanoTime();
+//        printTime(t1,t2,t3);
+
+        
+        
+        
+        
+        
+        
         
         
     }
+    
     
     private static void printTime(long t1, long t2, long t3){
         //System.out.println("Tiempo en generar Estado: " + (t2 - t1)/1000000 + ". Tiempo en SA con Coste: " + (t3 - t2)/1000000);
